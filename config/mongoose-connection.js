094e0f22e6c675c152526234ef415b16baa4c0ab
wardaowais <wardaowais123@gmail.com>
2024-11-05 00:24:05 +0500

    owner create env variables setup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
+
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
-   console.log("connected")
+   dbgr("connected")
+   console.log("reach")
 })
 .catch(function(err){
     console.log(err)
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,36 @@
-const express  = require('express')
+const express = require('express')
 const router = express.Router();
+const ownerModel = require("../models/owner-model")
 
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
     res.send("hey it is working")
 })
+
+if (process.env.NODE_ENV === "development") {
+    router.post("/create", async function (req, res) {
+
+        let owners = await ownerModel.find()
+        if (owners.length > 0) {
+            return res
+                .status(503)
+                .send("Ypu dont have permision to create a new owner")
+        }
+        let {fullname,email,password} = req.body
+        let createdOwner =  await ownerModel.create({
+            fullname,
+            email,
+            password,
+           
+            
+    
+        });
+        res.status(201).send(createdOwner)
+    })
+}
+
+
+
+
 
 module.exports = router;
 
