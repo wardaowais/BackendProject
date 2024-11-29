aaeb99bb01a9293c5056cfd33fa12fc602cbe58f
wardaowais <wardaowais123@gmail.com>
2024-11-29 10:34:47 -0800

    protected routes compelete

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ const ownerModel = require("./models/owner-model");
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+app.use(
+    expressSession({
+        resave: false,
+        saveUninitialized:false,
+        secret : process.env.Express_SESSION_SECRET,
+
+    })
+
+)
+app.use(flash());
+
 app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs")
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-const express = require("express")
+const express = require("express");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 const router = express.Router();
 
 router.get("/",function(req,res){
-    res.render("index")
+    let error = req.flash("error");
+    res.render("index",{error})
     
 });
+router.get("/shop", isLoggedIn, function(req,res){
+    res.render("shop")
+    
+});
+
 
 module.exports = router;
 
